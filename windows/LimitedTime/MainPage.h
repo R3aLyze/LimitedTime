bbf9b38e97aa125b073cd8a41fc9c62dbf493878
R3aLyze <jasjim55@gmail.com>
2024-08-08 23:23:11 -0400

    Initial commit This initial commit includes the template from reeact native

---
+++ windows/LimitedTime/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::LimitedTime::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::LimitedTime::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
